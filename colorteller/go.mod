03647beeae9b6f1fd21f729a2213d3781ef0fc6d
Jason Wang <7304774+jasonwzm@users.noreply.github.com>
2024-09-14 22:42:40 -0700

    update with exposure checks

--- colorteller/go.mod
+++ colorteller/go.mod
@@ -1,3 +1,5 @@
 module github.com/demo/colorteller
 
-go 1.12
+go 1.22
+
+require golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
--- colorteller/main.go
+++ colorteller/main.go
@@ -5,10 +5,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"golang.org/x/exp/rand"
 )
 
 const defaultPort = "8080"
 const defaultColor = "green"
+const defaultGroup = "control"
+
+func getStatsigServerSDKKey() string {
+	key := os.Getenv("STATSIG_SERVER_SDK_KEY")
+	if key == "" {
+		log.Println("Error: STATSIG_SERVER_SDK_KEY not set")
+		return ""
+	}
+	return key
+}
 
 func getServerPort() string {
 	port := os.Getenv("SERVER_PORT")
@@ -28,9 +41,43 @@ func getColor() string {
 	return defaultColor
 }
 
+func getGroup() string {
+	group := os.Getenv("GROUP")
+	if group != "" {
+		return group
+	}
+
+	return defaultGroup
+}
+
 type colorHandler struct{}
 
 func (h *colorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	sdkKey := getStatsigServerSDKKey()
+	if sdkKey != "" {
+		client := &http.Client{}
+		uid := fmt.Sprintf("%s-%s-%s-%s-%s",
+			fmt.Sprintf("%x", rand.Uint32()),
+			fmt.Sprintf("%x", rand.Uint32()),
+			fmt.Sprintf("%x", rand.Uint32()),
+			fmt.Sprintf("%x", rand.Uint32()),
+			fmt.Sprintf("%x", rand.Uint32()),
+		)
+		exposureData := fmt.Sprintf(`{"exposures": [{"user": {"customIDs": {"requestID": "%s"}}, "experimentName": "colorapp", "group": "%s"}]}`, uid, getGroup())
+		req, err := http.NewRequest("POST", "https://events.statsigapi.net/v1/log_custom_exposure", strings.NewReader(exposureData))
+		if err != nil {
+			log.Printf("Error creating request: %v", err)
+		} else {
+			req.Header.Set("statsig-api-key", sdkKey)
+			req.Header.Set("Content-Type", "application/json")
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Printf("Error sending exposure event: %v", err)
+			} else {
+				defer resp.Body.Close()
+			}
+		}
+	}
 	fmt.Fprint(writer, getColor())
 }
 
